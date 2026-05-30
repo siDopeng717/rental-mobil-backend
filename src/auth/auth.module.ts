f958ba687db1cbe578c34d62b833e2ad33d55957
siDopeng717 <caraka.a.putra@gmail.com>
2026-05-30 14:26:40 +0700

    fix jwt auth strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
@@ -15,7 +23,11 @@ import { JwtStrategy } from './jwt/jwt.strategy';
       },
     }),
   ],
+
   controllers: [AuthController],
+
   providers: [AuthService, JwtStrategy],
+
+  exports: [PassportModule, JwtModule],
 })
 export class AuthModule {}
