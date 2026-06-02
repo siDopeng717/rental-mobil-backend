d8fde5d6aab0ac42c2119ad302511ddba51feeca
siDopeng717 <caraka.a.putra@gmail.com>
2026-06-02 14:28:01 +0700

    fix swagger deployment

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,24 +19,41 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
-
-  // Swagger Config
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Car Rental API')
-    .setDescription('Documentation for Car Rental Backend API')
+    .setDescription(
+      'Documentation for Car Rental Backend API',
+    )
     .setVersion('1.0')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
 
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
 
-  console.log('Server running on http://localhost:3000');
-  console.log('Swagger running on http://localhost:3000/docs');
+  await app.listen(port);
+
+  console.log(
+    `Server running on http://localhost:${port}`,
+  );
 }
 
 bootstrap();
